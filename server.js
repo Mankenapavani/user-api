1314c64f273fe4ab140c73eb2564bf26db48e3ec
mankena pavani <mankenapavanireddy@gmail.com>
2026-03-26 11:54:51 +0530

    fixed for vercel

--- server.js
+++ server.js
@@ -105,8 +105,4 @@ app.delete('/users/:id', (req, res) => {
 });
 
 // ✅ IMPORTANT FOR DEPLOYMENT
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
